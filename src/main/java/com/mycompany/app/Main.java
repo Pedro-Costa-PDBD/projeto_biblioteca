aa6dd92e69a3a213e6cadbf0666971388cc91c2e
Pedro-Costa-PDBD <pedro.costa@pdbomdespacho.com.br>
2024-09-30 20:02:03 +0000

    chore: import do exercicio 2 e config do JUnit

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -17,5 +17,9 @@ public class Main {
         usuario.setIdade(25);
 
         Date data = new Date();
+
+        Pessoa pessoa = new Pessoa();
+        pessoa.nome_pessoa("Lucas Rodrigo");
+        pessoa.livros("Um dia no amanhecer, Pós primavera de outono, Tente rir antes que o mundo chore");
     }
 }
