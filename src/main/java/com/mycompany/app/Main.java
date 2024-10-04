f00ee40f0d98dcbcada4e15293a30a7ea795e150
Pedro-Costa-PDBD <pedro.costa@pdbomdespacho.com.br>
2024-10-04 23:29:53 +0000

    chore: upload dos arquivos do modulo 2 e config do JUnit

--- src/main/java/com/mycompany/app/Main.java
+++ src/main/java/com/mycompany/app/Main.java
@@ -3,25 +3,24 @@ import java.util.Date;
 public class Main {
     public static void main(String[] args) {
         Autor autor= new Autor();
-        Pessoa pessoa = new Pessoa();
-        pessoa.setNome_pessoa("João da Silva");
         autor.setNacionalidade("Brasileiro");
+        Pessoa pessoa = new Pessoa();
+        pessoa.setNome("João da Silva");
 
         Livro livro= new Livro();
         livro.setTitulo("Java para iniciantes");
         livro.setAutor(autor);
         livro.setGenero("Tecnologia");
-        livro.Validar_Disponivel();
+        livro.validarDisponibilidade();
 
         Usuario usuario= new usuario();
-        usuario.setNome("Bartolomeu");
         usuario.setIdade(25);
 
         Emprestimo emprestimo= new Emprestimo();
         emprestimo.setData_retirada(new Date());
         emprestimo.setData_devolucao(new Date());
         emprestimo.setLIvro(livro);
-        emprestimo.setUsuario(usuario);
+        emprestimo.setUSuario(usuario);
 
         System.out.println("Livro: " + livro.getTitulo());
         System.out.println("Autor: " + livro.getAutor().getNome_pessoa());
